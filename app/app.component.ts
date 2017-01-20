d343b1c53f284d4e839da3e3730b2736f76139b4
Jacob Hixon <jacobhixon47@gmail.com>
2017-01-19 20:10:22 -0800

    create Meal model at app/meal.model.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'app-root',
   template: `
-  <div class='container'>
-    <h1>Template</h1>
-  </div>
+    <div class='container'>
+      <div class='row'>
+        <meal-list></meal-list>
+      </div>
+    </div>
   `
 })
 
