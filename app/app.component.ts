12694d5144a6d71dedef9e200f012958c2d75cf8
Jacob Hixon <jacobhixon47@gmail.com>
2017-01-19 18:03:55 -0800

    setup Angular 2 Template

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class='container'>
+    <h1>Template</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
